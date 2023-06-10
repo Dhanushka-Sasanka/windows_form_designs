6280ea0670e230b0688dc50463c82d4d0c4426de
Dhanushka-Sasanka <dhanushkasasankapayza@gmail.com>
2023-06-10 15:35:05 +0530

    Add project files.

---
+++ WindowsFormsApp1/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+	public class AppDbContext : DbContext
+	{
+
+		public AppDbContext() : base(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString)
+		{ 
+		
+		}
+
+		public DbSet<Employee> EmployeeTable { get; set; }
+
+		
+	}
+}
---
+++ WindowsFormsApp1/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+	public class Employee
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }	
+
+		public string Address { get; set; }
+		public string Salary { get; set; }
+
+
+	}
+}
